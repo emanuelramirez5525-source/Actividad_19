c598d7a2b139be8f75e361c8aee1638afccba75f
emanu <emanu@EMANUEL_RAMREZ>
2026-03-22 22:36:21 -0600

    Logica Aplicada!

--- 7/Program.cs
+++ 7/Program.cs
@@ -1,3 +1,25 @@
 ﻿// Desarrolle un programa que solicite el salario de un empleado, validando que sea un número decimal válido mayor o igual a 0
 
+double salario;
 
+bool correcto;
+
+do
+{
+    Console.Clear();
+
+    Console.WriteLine("Ingrese un salario");
+    correcto = double.TryParse(Console.ReadLine(), out salario);    
+
+    if (!correcto || salario < 0)
+    {
+        Console.WriteLine("Salario invalido ...");
+        correcto=false;
+    }
+
+    Console.ReadKey();
+
+} while (!correcto);
+
+Console.WriteLine("Salario valido: " + salario);
+Console.ReadKey();
