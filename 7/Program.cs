996b252a7553068754e17e5cb2151b915390d2d7
emanu <emanu@EMANUEL_RAMREZ>
2026-03-22 22:18:19 -0600

    Programa Creado!

--- 7/Program.cs
+++ 7/Program.cs
@@ -1,2 +1,3 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Desarrolle un programa que solicite el salario de un empleado, validando que sea un número decimal válido mayor o igual a 0
+
+
