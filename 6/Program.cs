2e749cc23883ccd22055e6bcfa89322268af02cf
emanu <emanu@EMANUEL_RAMREZ>
2026-03-22 22:16:49 -0600

    Logica Aplicada!

--- 6/Program.cs
+++ 6/Program.cs
@@ -1,2 +1,24 @@
 ﻿//Desarrolle un programa que solicite una fecha, validando que el formato ingresado sea correcto, y luego mostrarla en formato corto
 
+DateTime fecha;
+bool correcto;
+
+do
+{
+    Console.Clear();
+
+    Console.WriteLine("Ingrese una fecha");
+    correcto = DateTime.TryParse(Console.ReadLine(), out fecha);
+
+    if (!correcto)
+    {
+        Console.WriteLine("Fecha invalida...");
+    }
+
+    Console.ReadKey();
+
+
+} while (!correcto);
+
+Console.WriteLine("Fecha corta " + fecha.ToShortDateString());
+Console.ReadKey();
