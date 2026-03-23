9c7503d8db4c39956c4bf2f0a43a6b76797ce210
emanu <emanu@EMANUEL_RAMREZ>
2026-03-22 21:05:30 -0600

    Logica Aplicada!

--- 2/Program.cs
+++ 2/Program.cs
@@ -1,3 +1,25 @@
 ﻿//Desarrolle un programa que solicite el precio de un producto, validando que sea un número decimal mayor que 0, repitiendo hasta que el usuario ingrese correctamente el valor
 
+double precio;
+bool correcto;
 
+do
+{
+    Console.Clear();
+    Console.WriteLine("Ingrese precio: ");
+    correcto = double.TryParse(Console.ReadLine(), out precio);
+    
+    if (!correcto || precio <= 0)
+    {
+        Console.WriteLine("Precio invalido...");
+        correcto = false;
+    }
+
+    Console.ReadKey();
+
+
+
+}while (!correcto);
+
+Console.Clear();
+Console.WriteLine("Precio valido: " + precio  );
