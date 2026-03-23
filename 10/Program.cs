081df0a098366e92979197fe256a121f5370bd06
emanu <emanu@EMANUEL_RAMREZ>
2026-03-22 20:25:29 -0600

    Programa Creado!

--- 10/Program.cs
+++ 10/Program.cs
@@ -1,1 +1,1 @@
-﻿// Desarrolle un programa que solicite la edad de una persona, validando que el valor ingresado sea un número entero válido, repitiendo hasta que el dato sea correcto y mostrando la edad ingresada
+﻿
