18e363d9f1b5ab2b5a53a90821ee01a66a4631dd
emanu <emanu@EMANUEL_RAMREZ>
2026-03-22 21:12:40 -0600

    Logica Aplicada!

--- 3/Program.cs
+++ 3/Program.cs
@@ -1,2 +1,25 @@
 ﻿// Desarrolle un programa que solicite la cantidad de estudiantes en un curso, validando que sea un número entero positivo, evitando valores negativos o letras
 
+int cantidad;
+bool correcto;
+
+do
+{
+    Console.Clear();
+    Console.WriteLine("Ingrese la cantidad");
+    correcto = int.TryParse(Console.ReadLine(), out cantidad);
+
+    if (correcto || cantidad <= 0)
+    {
+        Console.WriteLine("Cantidad invalida...");
+        correcto = false;
+    }
+
+    Console.ReadKey();
+
+
+
+} while (!correcto);
+
+Console.WriteLine("La cantidad de estudiantes es: " + cantidad);
+Console.ReadKey();  
