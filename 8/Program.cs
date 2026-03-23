3decd8afbc07db1e9e79245e404afff223ee5bb2
emanu <emanu@EMANUEL_RAMREZ>
2026-03-22 22:50:18 -0600

    Logica Aplicada!

--- 8/Program.cs
+++ 8/Program.cs
@@ -1,2 +1,23 @@
 ﻿//Desarrolle un programa que solicite un número y calcule su raíz cuadrada, validando que el valor ingresado sea numérico y no negativo
 
+double numero;
+bool correcto;
+
+do
+{
+    Console.Clear();
+    Console.WriteLine("Ingresa un número");
+    correcto = double.TryParse(Console.ReadLine(), out numero);
+
+    if (! correcto || numero < 0)
+    {
+        Console.WriteLine("Número invalido..");
+        correcto = false;
+    }
+
+    Console.ReadKey();
+
+} while (!correcto);
+
+Console.WriteLine("Raiz: " + Math.Sqrt (numero));
+Console.ReadKey();
