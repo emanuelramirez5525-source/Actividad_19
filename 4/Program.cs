13dd68ad674d5f994ce4ef016c1c92291fffc010
emanu <emanu@EMANUEL_RAMREZ>
2026-03-22 21:49:17 -0600

    Logica Aplicada!

--- 4/Program.cs
+++ 4/Program.cs
@@ -1,1 +1,26 @@
 ﻿// Desarrolle un programa que solicite el año de nacimiento de una persona, validando que sea un número válido y que esté en un rango lógico (por ejemplo entre 1900 y el año actual)
+
+int anio;
+int actual = DateTime.Now.Year;
+bool correcto;
+
+do
+{
+    Console.Clear();
+    Console.WriteLine("Ingrese fecha de nacimiento");
+    correcto = int.TryParse(Console.ReadLine(), out anio);    
+    
+    if (!correcto || anio < 1900 || anio > actual)
+    {
+        Console.WriteLine("Año invalido...");
+        correcto = false;
+    }
+
+    Console.ReadKey();
+
+
+
+}while (!correcto);
+
+Console.WriteLine("Año valido: " + anio);
+Console.ReadKey();
