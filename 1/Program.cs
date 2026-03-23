04b00901a02b06cd58f7e949b55c9d262a8520a6
emanu <emanu@EMANUEL_RAMREZ>
2026-03-22 20:37:20 -0600

    Logica Aplicada!

--- 1/Program.cs
+++ 1/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Desarrolle un programa que solicite la edad de una persona, validando que el valor ingresado sea un número entero válido, repitiendo hasta que el dato sea correcto y mostrando la edad ingresada
+int edad;
+bool correcto;
+
+do
+{
+    Console.Clear();
+    Console.WriteLine("Ingrese su edad");
+    correcto = int.TryParse(Console.ReadLine(), out edad );
+
+    if (!correcto || edad < 0)
+    {
+        Console.WriteLine("Edad invalida.. vuelve a intentar");
+        correcto=false;
+
+    }
+
+    Console.ReadKey();
+
+
+} while (!correcto);
+
+Console.Clear();
+Console.WriteLine("Edad es igual: " +  edad);
+Console.ReadKey();
