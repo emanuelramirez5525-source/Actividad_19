5d0596aeb736d8d8b369d40296c66cfe2c01c3a9
emanu <emanu@EMANUEL_RAMREZ>
2026-03-22 23:05:59 -0600

    Logica Aplicada!

--- 10/Program.cs
+++ 10/Program.cs
@@ -1,1 +1,31 @@
 ﻿//Desarrolle un programa que solicite la cantidad de productos a comprar, validando que sea un número entero positivo y luego simule el ingreso de esos productos
+
+int cantidad;
+bool correcto;
+
+do
+{
+
+    Console.Clear();
+    Console.WriteLine("Ingrese cantidad de producto ");
+    correcto = int.TryParse(Console.ReadLine(), out cantidad);  
+    
+    if (!correcto || cantidad <= 0)
+    {
+        Console.WriteLine("Cantidad invalida... ");
+        correcto = false;
+    }
+
+    Console.ReadKey();
+
+
+
+} while (!correcto);
+
+for (int i = 1; i <= cantidad; i++)
+{
+    Console.WriteLine("Producto " + i + ":");
+    Console.ReadLine();
+}
+
+Console.ReadKey();
--- 9/Program.cs
+++ 9/Program.cs
@@ -1,2 +1,32 @@
 ﻿// Desarrolle un programa que solicite la nota de un estudiante, validando que sea un número entre 0 y 100 antes de mostrar si aprueba o reprueba
 
+double nota;
+
+bool correcto;
+
+do
+{
+    Console.Clear();
+    Console.WriteLine("Ingrese nota");
+    correcto = double.TryParse(Console.ReadLine(), out nota);
+    if (!correcto || nota < 0 || nota > 100)
+    {
+        Console.WriteLine( "Nota invalida...");
+        correcto = false;
+    }
+    Console.ReadKey();  
+
+
+
+} while (!correcto);
+
+if (nota >= 61)
+{
+    Console.WriteLine("Aprueba");
+}
+else
+{
+    Console.WriteLine("Reprueba");
+}
+
+Console.ReadKey();
